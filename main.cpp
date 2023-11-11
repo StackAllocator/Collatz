ec1e787dc0b6bb04557dc15c8d51ff0952c244df
Eldinur <134613763+StackAllocator@users.noreply.github.com>
2023-11-11 20:15:05 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
 int Collatz(int num) {
@@ -35,12 +12,11 @@ int Collatz(int num) {
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main(void)
-{
+int main() {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 2000;
-    const int screenHeight = 1400;
+    constexpr int screenWidth = 2000;
+    constexpr int screenHeight = 1400;
     int distanceX = 20;
     int distanceY = 20;
     int a = 60;
@@ -136,8 +112,6 @@ int main(void)
             EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
