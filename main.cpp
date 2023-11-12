03cfec3905bbb249ac75f49475efc2054c48cd8e
Johannes <Johannes.Elsing@gmx.de>
2023-11-12 13:24:48 +0100

    added switch and made some refactoring

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
+#include <iostream>
 #include "raylib.h"
-#include <stdio.h>
 
 int Collatz(int num) {
     if (num == 1) {
@@ -10,44 +10,227 @@ int Collatz(int num) {
         return 3 * num + 1;
     }
 }
+void checkInputs(int &fps, float &distanceX, float &distanceY,
+    float &zoomMultX, float &zoomMultY, float &offsetX, float &offsetY,
+    int &start, float &speed, int &colorVal, int &anim);
+
+
+enum class DrawOneStates {
+  GET_NUMBER,
+  DRAW_LINE,
+  END_SCREEN,
+};
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main() {
+auto main() -> int {
     // Initialization
     //--------------------------------------------------------------------------------------
     constexpr int screenWidth = 1920;
     constexpr int screenHeight = 1080;
-    int distanceX = 20;
-    int distanceY = 20;
-    int a = 200;
+    float distanceX = 20;
+    float distanceY = 20;
+    int colorVal = 200;
+
+	// set default DrawOneStates value in case only one series should be drawn
+	DrawOneStates drawOneState = DrawOneStates::GET_NUMBER;
+	int numToCheckOnCollatz{};
+	int key = 0;
+	int count = 0;
+	std::string inputString = "";
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Collatz Conjecture Visualization");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     int fps = 0;
     int anim = 2;
     bool all = false;
     int s_max = 1;
-    Color colors[a*10];
-    int zoomMultX = 1;
-    int zoomMultY = 1;
-    int offsetX = 0;
-    int offsetY = 0;
+    Color colors[colorVal * 10];
+    float zoomMultX = 1;
+    float zoomMultY = 1;
+    float offsetX = 0;
+    float offsetY = 0;
     int start = 1;
-    int speed = 1;
+    float speed = 1.f;
+    Rectangle drawOneRect = {screenWidth - 200, screenHeight - 300, 150, 50};
+    Color rectCol = RED;
+	bool drawOne = false;
+
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {        
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
         fps = ++fps % 1;
-        // Draw
         //----------------------------------------------------------------------------------
+
+		Vector2 currentMousePos = { static_cast<float>(GetMouseX()), static_cast<float>(GetMouseY()) };
+        if (currentMousePos.x >= drawOneRect.x && currentMousePos.x <= drawOneRect.x + drawOneRect.width
+            && currentMousePos.y >= drawOneRect.y && currentMousePos.y <= drawOneRect.y + drawOneRect.height) {
+          rectCol = BLUE;
+		  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+			drawOne = true;
+		  }
+        }
+        else {
+          rectCol = RED;
+        }
+        DrawRectangleRec(drawOneRect, rectCol);
+		DrawText("Press me!", drawOneRect.x + 5, drawOneRect.y + 12, 30, BLACK);
+
+        checkInputs(fps, distanceX, distanceY, zoomMultX, zoomMultY, offsetX, offsetY, start, speed, colorVal, anim);
+            BeginDrawing();
+                
+                ClearBackground(RAYWHITE);
+                DrawLine(30, 0, 30, screenHeight, BLACK);
+
+                for (int i = screenHeight - 30; i >= 0; i -= distanceX) {
+                    int n = (int)((screenHeight-30) * zoomMultX)/distanceX - (i * zoomMultX)/distanceX + (offsetY * zoomMultX);     
+                    DrawText(TextFormat("%d", n), 5, i - 5, 15, BLACK);
+                    DrawLine(25, i, 35, i, BLACK);
+
+                }
+                DrawLine(0, screenHeight - 30, screenWidth, screenHeight - 30, BLACK);
+                for (int i = 0; i < screenWidth; i+=distanceY) {
+                    int n = (int)((i * zoomMultY)/distanceY) + (offsetX * zoomMultY);     
+                    DrawText(TextFormat("%d", n), i-5 + 30, screenHeight - 20, 15, BLACK);
+                    DrawLine(i + 30, screenHeight-25, i + 30, screenHeight-30, BLACK);
+                }
+                
+                Vector2 pos;
+                pos.x = 30;
+                pos.y = screenHeight - 30;
+                int num = 0;
+                Color col;
+                col.r = GetRandomValue(0, 255);
+                col.g = GetRandomValue(0, 255);
+                col.b = GetRandomValue(0, 255);
+                col.a = 255; // alpha value
+
+                colors[anim] = col;
+
+				if (!drawOne) {
+				  for (int i = start; i < anim; i++) {
+                    num = i;
+                    int count = 0;
+                    int old_x = 30 - (distanceY * offsetX / zoomMultY);
+                    int old_y = screenHeight - 30 - num * distanceX / zoomMultX + (distanceX * offsetY / zoomMultX);
+                    pos.y = old_y;
+                    pos.x = old_x;
+                    do {
+                        num = Collatz(num);
+                        count++;
+                        old_x = pos.x;
+                        old_y = pos.y;
+                        pos.y = screenHeight - 30 - distanceX * num / zoomMultX + (offsetY * distanceX / zoomMultX);
+                        pos.x = 30 + count * distanceY / zoomMultY - (offsetX * distanceY / zoomMultY);
+                        DrawLine(old_x, old_y, pos.x, pos.y, colors[i]);
+                    } while ((num > 1) && ((count < s_max) || (i < anim - 1)));
+                    
+				  }
+                
+				  if (num == 1 && anim < colorVal) {
+                    anim++;
+                    s_max = 0;
+				  }
+				  pos.x = 30;
+				  pos.y = screenHeight-30;
+				  for (int i = start; i < anim; i++) {
+                    num = i;
+                    int count = 0;
+                    pos.x = 30 - ((offsetX) * distanceY / zoomMultY);
+                    pos.y = screenHeight - 30 - num * distanceX / zoomMultX + (offsetY * distanceX / zoomMultX);
+                    do {
+                        num = Collatz(num);
+                        count++;
+                        DrawCircle(pos.x, pos.y, 3, colors[i]);
+                        pos.y = screenHeight - 30 - distanceX * num / zoomMultX + (offsetY * distanceX / zoomMultX);
+                        pos.x = 30 + count * distanceY / zoomMultY - (offsetX * distanceY / zoomMultY);
+                        
+                        
+                    } while ((num > 1) && ((count < s_max) || (i < anim - 1))); 
+                    DrawCircle(pos.x, pos.y, 3, colors[i]);
+				  }
+                
+				  DrawLine(30, 0, 30, screenHeight, BLACK);
+				  for (int i = screenHeight - 30; i >= 0; i-=distanceX) {
+                    int n = static_cast<int>((screenHeight-30) * zoomMultX)/distanceX-(i * zoomMultX)/distanceX + (offsetY * zoomMultX);     
+                    DrawText(TextFormat("%i", n), 5, i-5, 15, BLACK);
+                    DrawLine(25, i, 35, i, BLACK);
+                   
+				  }
+				  DrawLine(0, screenHeight - 30, screenWidth, screenHeight - 30, BLACK);
+				  for (int i = 0; i < screenWidth; i+=distanceY) {
+                    int n = (int)((i * zoomMultY)/distanceY) + (offsetX * zoomMultY);     
+                    DrawText(TextFormat("%i", n), i-5 + 30, screenHeight - 20, 15, BLACK);
+                    DrawLine(i + 30, screenHeight-25, i + 30, screenHeight-30, BLACK);
+				  }
+				}
+
+				else {
+				  switch(drawOneState) {
+					// get user input
+				  case DrawOneStates::GET_NUMBER:
+					// numToCheckOnCollatz;
+					while (key != KEY_ENTER) {
+					  key = GetKeyPressed();
+					  if (key >= KEY_ZERO && key <= KEY_NINE) {
+						inputString += key;
+						++count;
+					  }
+					}
+					break;
+					// case where one number gets drawn
+				  case DrawOneStates::DRAW_LINE: [[likely]]
+
+					// numToCheckOnCollatz = std::atoi(inputString);
+					if (numToCheckOnCollatz == 1) {
+					  drawOneState = DrawOneStates::END_SCREEN;
+					}
+					break;
+				  case DrawOneStates::END_SCREEN:
+					break;
+				  default:
+					break;
+				  }
+				}
+
+                s_max+=speed;
+                int txtpos = 200;
+
+				// -----------------------------------------------------------------------------------------------------------------------------------
+				// display test
+                DrawText(TextFormat("FPS: %i", fps), screenWidth - txtpos, 50, 20, BLACK);
+                DrawText(TextFormat("s_max: %i", s_max), screenWidth - txtpos, 100, 20, BLACK);
+                DrawText(TextFormat("anim: %i/%i", anim, colorVal), screenWidth - txtpos, 150, 20, BLACK);
+                DrawText(TextFormat("start: %i <> %i", start, colorVal), screenWidth - txtpos, 200, 20, BLACK);
+                DrawText(TextFormat("zoomMultX: %i", zoomMultX), screenWidth - txtpos, 250, 20, BLACK);
+                DrawText(TextFormat("zoomMultY: %i", zoomMultY), screenWidth - txtpos, 300, 20, BLACK);
+                DrawText(TextFormat("offsetX: %i", offsetX), screenWidth - txtpos, 350, 20, BLACK);
+                DrawText(TextFormat("offsetY: %i", offsetY), screenWidth - txtpos, 400, 20, BLACK);
+                DrawText(TextFormat("color: (%d, %d, %d)", col.r, col.g, col.b), screenWidth - txtpos, 450, 20, BLACK);
+                DrawText(TextFormat("num: %i", num), screenWidth - txtpos, 500, 20, BLACK);
+                DrawText(TextFormat("Speed: %i", speed), screenWidth - txtpos, 550, 20, BLACK);
+				// -----------------------------------------------------------------------------------------------------------------------------------
+
+            EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+
+void checkInputs(int &fps, float &distanceX, float &distanceY,
+    float &zoomMultX, float &zoomMultY, float &offsetX, float &offsetY,
+    int &start, float &speed, int &colorVal, int &anim) {
             if (IsKeyDown(KEY_UP) && fps == 0) {
                 distanceX++;
                 if (distanceX >= 80 && zoomMultX > 1) {
@@ -60,6 +243,7 @@ int main() {
                     distanceX = 80;
                 }
                 
+        // Draw
             } else if (IsKeyDown(KEY_DOWN) && fps == 0) {
                 distanceX--;
                 if (distanceX <= 40) {
@@ -86,11 +270,11 @@ int main() {
                     zoomMultY *= 2;
                 }
             }
-            if (IsKeyDown(KEY_PERIOD) && !IsKeyDown(KEY_LEFT_CONTROL) && a > 1) {
-                a--;
+            if (IsKeyDown(KEY_PERIOD) && !IsKeyDown(KEY_LEFT_CONTROL) && colorVal > 1) {
+                colorVal--;
             }
             if (IsKeyDown(KEY_COMMA) && !IsKeyDown(KEY_LEFT_CONTROL)) {
-                a++;
+                colorVal++;
             }
             if (IsKeyDown(KEY_PERIOD) && IsKeyDown(KEY_LEFT_CONTROL) && start > 1) {
                 start--;
@@ -118,110 +302,4 @@ int main() {
             if (IsKeyDown(KEY_S) && offsetY > 0) {
                 offsetY--;
             }
-            BeginDrawing();
-                
-                ClearBackground(RAYWHITE);
-                DrawLine(30, 0, 30, screenHeight, BLACK);
-                for (int i = screenHeight - 30; i >= 0; i-=distanceX) {
-                    int n = (int)((screenHeight-30) * zoomMultX)/distanceX-(i * zoomMultX)/distanceX + (offsetY * zoomMultX);     
-                    DrawText(TextFormat("%i", n), 5, i-5, 15, BLACK);
-                    DrawLine(25, i, 35, i, BLACK);
-
-                }
-                DrawLine(0, screenHeight - 30, screenWidth, screenHeight - 30, BLACK);
-                for (int i = 0; i < screenWidth; i+=distanceY) {
-                    int n = (int)((i * zoomMultY)/distanceY) + (offsetX * zoomMultY);     
-                    DrawText(TextFormat("%i", n), i-5 + 30, screenHeight - 20, 15, BLACK);
-                    DrawLine(i + 30, screenHeight-25, i + 30, screenHeight-30, BLACK);
-                }
-                
-                Vector2 pos;
-                pos.x = 30;
-                pos.y = screenHeight-30;
-                int num = 0;
-                Color col;
-                col.r = GetRandomValue(0, 255);
-                col.g = GetRandomValue(0, 255);
-                col.b = GetRandomValue(0, 255);
-                col.a = 255;
-                colors[anim] = col;
-                for (int i = start; i < anim; i++) {
-                    num = i;
-                    int count = 0;
-                    int old_x = 30 - (distanceY * offsetX / zoomMultY);
-                    int old_y = screenHeight - 30 - num * distanceX / zoomMultX + (distanceX * offsetY / zoomMultX);
-                    pos.y = old_y;
-                    pos.x = old_x;
-                    
-                    
-                    do {
-                        num = Collatz(num);
-                        count++;
-                        old_x = pos.x;
-                        old_y = pos.y;
-                        pos.y = screenHeight - 30 - distanceX * num / zoomMultX + (offsetY * distanceX / zoomMultX);
-                        pos.x = 30 + count * distanceY / zoomMultY - (offsetX * distanceY / zoomMultY);
-                        DrawLine(old_x, old_y, pos.x, pos.y, colors[i]);
-                    } while ((num > 1) && ((count < s_max) || (i < anim - 1)));
-                    
-                }
-                
-                if (num == 1 && anim < a) {
-                    anim++;
-                    s_max = 0;
-                }
-                pos.x = 30;
-                pos.y = screenHeight-30;
-                for (int i = start; i < anim; i++) {
-                    num = i;
-                    int count = 0;
-                    pos.x = 30 - (offsetX * distanceY / zoomMultY);
-                    pos.y = screenHeight - 30 - num * distanceX / zoomMultX + (offsetY * distanceX / zoomMultX);
-                    do {
-                        num = Collatz(num);
-                        count++;
-                        DrawCircle(pos.x, pos.y, 3, colors[i]);
-                        pos.y = screenHeight - 30 - distanceX * num / zoomMultX + (offsetY * distanceX / zoomMultX);
-                        pos.x = 30 + count * distanceY / zoomMultY - (offsetX * distanceY / zoomMultY);
-                        
-                        
-                    } while ((num > 1) && ((count < s_max) || (i < anim - 1))); 
-                    DrawCircle(pos.x, pos.y, 3, colors[i]);
-                }
-                
-                DrawLine(30, 0, 30, screenHeight, BLACK);
-                for (int i = screenHeight - 30; i >= 0; i-=distanceX) {
-                    int n = static_cast<int>((screenHeight-30) * zoomMultX)/distanceX-(i * zoomMultX)/distanceX + (offsetY * zoomMultX);     
-                    DrawText(TextFormat("%i", n), 5, i-5, 15, BLACK);
-                    DrawLine(25, i, 35, i, BLACK);
-                   
-                }
-                DrawLine(0, screenHeight - 30, screenWidth, screenHeight - 30, BLACK);
-                for (int i = 0; i < screenWidth; i+=distanceY) {
-                    int n = (int)((i * zoomMultY)/distanceY) + (offsetX * zoomMultY);     
-                    DrawText(TextFormat("%i", n), i-5 + 30, screenHeight - 20, 15, BLACK);
-                    DrawLine(i + 30, screenHeight-25, i + 30, screenHeight-30, BLACK);
-                }
-                
-                s_max+=speed;
-                int txtpos = 200;
-                DrawText(TextFormat("fps: %i",fps), screenWidth - txtpos, 50, 20, BLACK);
-                DrawText(TextFormat("s_max: %i",s_max), screenWidth - txtpos, 100, 20, BLACK);
-                DrawText(TextFormat("anim: %i/%i",anim, a), screenWidth - txtpos, 150, 20, BLACK);
-                DrawText(TextFormat("start: %i <> %i",start, a), screenWidth - txtpos, 200, 20, BLACK);
-                DrawText(TextFormat("zoomMultX: %i",zoomMultX), screenWidth - txtpos, 250, 20, BLACK);
-                DrawText(TextFormat("zoomMultY: %i",zoomMultY), screenWidth - txtpos, 300, 20, BLACK);
-                DrawText(TextFormat("offsetX: %i",offsetX), screenWidth - txtpos, 350, 20, BLACK);
-                DrawText(TextFormat("offsetY: %i",offsetY), screenWidth - txtpos, 400, 20, BLACK);
-                DrawText(TextFormat("color: %i, %i, %i", col.r, col.g, col.b), screenWidth - txtpos, 450, 20, BLACK);
-                DrawText(TextFormat("num: %i",num), screenWidth - txtpos, 500, 20, BLACK);
-                DrawText(TextFormat("speed: %i",speed), screenWidth - txtpos, 550, 20, BLACK);
-            EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
 }
